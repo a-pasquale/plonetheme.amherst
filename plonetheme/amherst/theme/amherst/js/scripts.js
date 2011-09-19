b22f86940b76c84680612fe3a20456d0ed244b63
Andrew Pasquale <andrew@elytra.net>
2011-09-19 09:18:15 -0400

    Overlay newsItems images.

--- plonetheme/amherst/theme/amherst/js/scripts.js
+++ plonetheme/amherst/theme/amherst/js/scripts.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
-  $("#drawer_tab").click(function(){
-    $("#wrap960").animate({
-      left: parseInt($("#wrap960").css('left'),10) == 300 ? 
-        0 : 300
-    });
-    $("#drawer").animate({
-      left: parseInt($("#drawer").css('left'),10) == 0 ? 
-        -$("#drawer").outerWidth() : 0
-    });
-    $("#drawer_tab").animate({
-      left: parseInt($("#drawer_tab").css('left'),10) == 0 ?
-        300 : 0
-    });
+  jq('.newsImageContainer a')
+    .prepOverlay({
+      subtype: 'image',
+      urlmatch: '/image_view_fullscreen$',
+      urlreplace: '_preview'
   });
 });
